e34f546f1a7e674367a14f8551c916511d90ad4f
Mirnes Zahirović <mirnes.zahiirovic99@gmail.com>
2026-04-06 12:31:38 +0200

    Remove unused script loading and comments
    - Eliminated the script loading for Plausible analytics in non-development environments to streamline the code.
    - Removed commented-out Typography components to enhance readability and maintainability.
    - This change reduces unnecessary overhead and improves the clarity of the preview configuration.

--- packages/material-react-table/.storybook/preview.tsx
+++ packages/material-react-table/.storybook/preview.tsx
@@ -45,53 +45,9 @@ const preview: Preview = {
         return () => channel.off(DARK_MODE_EVENT_NAME, setDark);
       }, [theme]);
 
-      useEffect(() => {
-        if (process.env.NODE_ENV === 'development') return;
-        const script = document.createElement('script');
-        script.src = 'https://plausible.io/js/script.js';
-        script.setAttribute('data-domain', 'material-react-table.dev');
-        script.defer = true;
-
-        document.body.appendChild(script);
-        return () => {
-          document.body.removeChild(script);
-        };
-      }, []);
-
       return (
         <ThemeProvider mode={isDark ? 'dark' : 'light'}>
           <LocalizationProvider dateAdapter={AdapterDayjs}>
-            {/* <Typography
-              sx={{
-                pb: '0.5rem',
-                color: useDarkMode() ? '#fff' : '#666',
-              }}
-              variant="subtitle2"
-            >
-              Looking for the main docs site? Click{' '}
-              <Link
-                href="https://www.material-react-table.com"
-                target="_blank"
-                rel="noopener"
-              >
-                here.
-              </Link>
-            </Typography>
-            <Typography
-              variant="subtitle2"
-              sx={{
-                pb: '1rem',
-                color: useDarkMode() ? '#fff' : '#666',
-              }}
-            >
-              View Source code for these examples in the code tab below or{' '}
-              <Link
-                href="https://github.com/KevinVandy/material-react-table/tree/v3/packages/material-react-table/stories/features"
-                target="_blank"
-              >
-                here on GitHub.
-              </Link>
-            </Typography> */}
             <Story {...context} />
           </LocalizationProvider>
         </ThemeProvider>
