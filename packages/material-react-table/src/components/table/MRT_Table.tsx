06c83ef7d1d9f93c5534fdae9167fc72fb2440fd
Ahmed Mehmedović <ahmedm.bjelave@gmail.com>
2026-06-25 12:44:31 +0200

    korekcija enablerowvirtualization

--- packages/material-react-table/src/components/table/MRT_Table.tsx
+++ packages/material-react-table/src/components/table/MRT_Table.tsx
@@ -21,6 +21,7 @@ export const MRT_Table = <TData extends MRT_RowData>({
     getState,
     options: {
       columns,
+      enableRowVirtualization,
       enableStickyHeader,
       enableTableFooter,
       enableTableHead,
@@ -66,6 +67,7 @@ export const MRT_Table = <TData extends MRT_RowData>({
       sx={(theme) => ({
         borderCollapse: 'separate',
         display: layoutMode?.startsWith('grid') ? 'grid' : undefined,
+        overflow: enableRowVirtualization ? undefined : 'auto',
         position: 'relative',
         ...(parseFromValuesOrFunc(tableProps?.sx, theme) as Record<
           string,
