83b32b9d77d04dcbcaa58cd877d161f960128873
zmirnes <mirnes.zahiirovic99@gmail.com>
2026-06-26 08:03:59 +0200

    REAC-1283: set table overflow to visible to prevent unwanted scrollbar

--- packages/material-react-table/src/components/table/MRT_Table.tsx
+++ packages/material-react-table/src/components/table/MRT_Table.tsx
@@ -66,7 +66,7 @@ export const MRT_Table = <TData extends MRT_RowData>({
       sx={(theme) => ({
         borderCollapse: 'separate',
         display: layoutMode?.startsWith('grid') ? 'grid' : undefined,
-        overflow: 'auto',
+        overflow: 'visible',
         position: 'relative',
         ...(parseFromValuesOrFunc(tableProps?.sx, theme) as Record<
           string,
